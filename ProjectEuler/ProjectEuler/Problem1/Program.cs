c8954346a1e0da6a853f679070cb56bac88bea09
Larry Gasik <larry@lgproductions.us>
2019-08-05 14:53:20 -0500

    Project Euler 1 Initial solution

---
+++ ProjectEuler/ProjectEuler/Problem1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+            //Find the sum of all the multiples of 3 or 5 below 1000.
+            int i = 3;
+            int sum = 0;
+            while (i < 1000)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                    sum += i;
+                i++;
+            }
+
+            Console.Write(sum.ToString());
+            Console.ReadKey();
+        }
+    }
+}
