39ce0bdb19f93dee361d2cd913fc6522cf37fe0c
Larry Gasik <larry@lgproductions.us>
2019-09-05 15:03:41 -0500

    Added Core Demonstration

---
+++ Tutorial/Core/MsIntro/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tutorial from 
+            //https://dotnet.microsoft.com/learn/dotnet/hello-world-tutorial/intro
+            
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("The Current Time is " + DateTime.Now);
+
+            List<string> myList = new List<string>();
+            myList.Add("Hello");
+            myList.Add("My Friend");
+            myList.Add("Hello");
+
+            foreach (string x in myList)
+            {
+                Console.WriteLine(x);
+            }
+            //Execute this by typing 
+            //dotnet run
+            //in the console
+        }
+    }
+}
