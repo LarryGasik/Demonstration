2b48723539bf05aaff08a2bdae7a047141ccc5f4
Larry Gasik <larry@lgproductions.us>
2019-09-09 09:13:08 -0500

    Added Rider Project

---
+++ Rider/ClassLibrary1/SomeService.Tests/TimeServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace SomeService.Tests
+{
+    [TestFixture]
+    public class TimeServiceTests
+    {
+        [Test]
+        public void WhenTimeIsEven()
+        {
+            DateTime candidate = new DateTime(2000, 1, 1, 1, 1, 4);
+            var result = TimeService.
+            Assert.AreEqual(true,);
+        }
+        [Test]
+        public void Odd()
+        {
+            Assert.True(true);
+        }
+        [Test]
+        public void WhenTimeIsZero()
+        {
+            Assert.True(true);
+        }
+    }
+}
