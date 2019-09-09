408d314d7ea753b0399a74182fa49648986854e1
Larry Gasik <larry@lgproductions.us>
2019-09-09 11:10:46 -0500

    wrote Application to read lines.

---
+++ Tutorial/TrimmedSelfContained/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace trimmedSelfContained
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"test.txt");
+
+            foreach (string line in lines)
+            {
+                Console.WriteLine("\t" + line);
+            }
+
+            Console.WriteLine("Press a key to exit.");
+            Console.Read();
+        }
+    }
+}
