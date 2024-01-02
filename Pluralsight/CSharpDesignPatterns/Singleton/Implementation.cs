763571df4d4e06251226c38d2033e74cdcbac2f6
Larry Gasik <larry@lgproductions.us>
2024-01-01 23:48:36 -0600

    Created Logger Implementation

---
+++ Pluralsight/CSharpDesignPatterns/Singleton/Implementation.cs
@@ -0,0 +1,26 @@
+﻿namespace Singleton;
+
+/// <summary>
+/// Singleton
+/// </summary>
+public class Logger
+{
+    private static Logger? _instance;
+    /// <summary>
+    ///  Clients cannot instantiate this
+    /// </summary>
+    protected Logger()
+    {
+
+    }
+
+    public static Logger Instance {
+        get
+        {
+            if( _instance == null )
+                _instance = new Logger();
+
+            return _instance;
+        }
+    }
+}
