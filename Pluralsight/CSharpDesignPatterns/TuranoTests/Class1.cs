f9998a38ec2a87c2f76b86937c86ca338cabd6f9
Larry Gasik <larry@lgproductions.us>
2024-01-10 07:16:51 -0600

    Did the Factory Method

---
+++ Pluralsight/CSharpDesignPatterns/TuranoTests/Class1.cs
@@ -0,0 +1,22 @@
+﻿namespace TuranoTests
+{
+    public class Class1()
+    {
+        public decimal BusinessMethod(bool isTuesday, string accountCode, string role, bool isLucky, string state)
+        {
+            if (role == "admin" || (role == "student" && isTuesday))
+                return 100;
+            if (isLucky && (isTuesday || state == "IL"))
+            {
+                return 2;
+            }
+            return 0;
+        }
+    }
+
+    public class someServiceObject
+    {
+        public bool isConfirmed { get; set; }
+        Guid id { get; set; }
+    }
+}
---
+++ Pluralsight/CSharpDesignPatterns/TuranoTests/test.cs
@@ -0,0 +1,65 @@
+﻿using Moq;
+using NUnit.Framework;
+
+namespace TuranoTests;
+
+[TestFixture]
+public class test()
+{
+    private Class1 _sut;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _sut = new Class1();
+    }
+    [Test]
+    public void TestMethod()
+    {
+        var result = _sut.BusinessMethod(true, "AAA", "admin", false, "IL");
+        Assert.That(result, Is.EqualTo(100));
+    }
+    
+    [Test]
+    public void TestMethodfalse()
+    {
+        var result = _sut.BusinessMethod(true, "AAA", "false", false, "IL");
+        Assert.That(result, Is.EqualTo(0));
+    }
+
+    [Test]
+    public void TestMethodMock()
+    {
+        var result = _sut.BusinessMethod(It.IsAny<bool>(), It.IsAny<string>(), "admin", It.IsAny<bool>() , It.IsAny<string>());
+        Assert.That(result, Is.EqualTo(100));
+    }
+
+    [Test]
+    public void TestMethodMock2()
+    {
+        var result = _sut.BusinessMethod(true, It.IsAny<string>(), "admin", It.IsAny<bool>(), It.IsAny<string>());
+        Assert.That(result, Is.EqualTo(100));
+    }
+
+    public void TestMethodMock3()
+    {
+        var result = _sut.BusinessMethod(false, It.IsAny<string>(), "admin", It.IsAny<bool>(), It.IsAny<string>());
+        Assert.That(result, Is.EqualTo(100));
+    }
+    [Test]
+    public void TestMethodMock4()
+    {
+        var result = _sut.BusinessMethod(true, "BBB", "admin", false, "NY");
+        Assert.That(result, Is.EqualTo(100));
+    }
+    [Test]
+    public void TestMethodMock5()
+    {
+        var result = _sut.BusinessMethod(true, "AAA", "admin", true, "IL");
+        Assert.That(result, Is.EqualTo(2));
+    }
+    
+    
+    
+    
+}
